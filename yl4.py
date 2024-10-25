b8ee88086eafd7e98e4a3da55205190a6d4dac54
marius <mariuskendra20@gmail.com>
2024-10-25 11:44:09 +0300

    yl4 and yl6

---
+++ yl4.py
@@ -0,0 +1,8 @@
+a = int("sesesta arv: ")
+b = int("sesesta teine arv: ")
+
+if a > b:
+    print(b, "on miinimum")
+
+else:
+    print(a, "on miinimum")
---
+++ yl6.py
@@ -0,0 +1,14 @@
+# kirjuta program, mis leiab kolmest kasutaja poolt sisestatud arvust maksimumi (ära kasuta max funkttsiooni)
+
+n1 = int(input("sisesta esimene arv: "))
+n2 = int(input("sisesta teine arv: "))
+n3 = int(input("sisesta kolmas arv: "))
+
+if n1 > n2 and n1 > n3:
+    print(n1, "on maksimum")
+
+elif n2 > n3:
+    print(n2, "on maksimum") 
+
+else:
+    print(n3, "on maksimum")
